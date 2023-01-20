31ecc17eca1f2ad55f2af8ec14279743837c0a09
Kyosuke Kobashigawa <e225706@ie.u-ryukyu.ac.jp>
2023-01-20 18:21:01 +0900

    2nd

---
+++ janken/src/jp/ac/uryukyu/ie/e225706/janken.java
@@ -0,0 +1,122 @@
+package jp.ac.uryukyu.ie.e225706;
+
+import java.util.*;
+
+abstract class Character{
+    private String name;
+    private int win;
+    private int lose;
+
+    private ArrayList<Hand> hands = new ArrayList<>();
+
+    Character(String name, int win, int lose){
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+    }
+    
+    void addAction(Hand hand) {
+        hands.add(hand);
+    }
+
+    void openStatus(){
+        System.out.printf("%s:win %d  lose %d\n", name, win, lose);
+    }
+
+    abstract void act(ArrayList<Character> targets);
+
+    String getName(){
+        return this.name;
+    }
+    int getWin(){
+        return this.win;
+    }
+    int getLose(){
+        return this.lose;
+    }
+
+    ArrayList<Hand> getHands(){
+        return hands;
+    }
+
+    void setWin(int win){
+        this.win = win;
+    }
+    void setLose(int lose){
+        this.lose = lose;
+    }
+}
+
+class Player extends Character{
+
+    Player(String name, int win, int lose){
+        super(name, win, lose);
+    }
+
+    @Override
+    void act(ArrayList<Character> targets) {
+        var command_selector = new CommandSelector();
+        
+       //選択肢を用意する
+        for(var action: getHands()) {
+            command_selector.addCommand(action.name());
+        }
+       //ユーザの選択を待つ
+        var command_number = command_selector.waitForUsersCommand("最初はグー、ジャンケン...");
+        System.out.println("ポンッ！");
+        getHands().get(command_number);
+    }
+}
+
+ interface Hand {
+    String name();
+ }
+
+ class CommandSelector {
+    ArrayList<String> commands;
+    Scanner scanner;
+
+    CommandSelector() {
+        scanner = new Scanner(System.in);
+        commands = new ArrayList<>();
+    }
+
+    void addCommand(String command_name) {
+        commands.add(command_name);
+    }
+
+    //promptを表示した上で，ユーザの選択を待つ
+    int waitForUsersCommand(String prompt) {
+        var index = 0;
+        System.out.println(prompt);
+        for(var command : commands) { //選択肢をprint
+            System.out.println(index + ":" + command);
+            index += 1;
+        }
+
+        //標準入力から数値を入力
+        while(true) {
+            int target_index = scanner.nextInt();
+
+            if (target_index >= 0 && target_index < commands.size()) {
+                return target_index;
+            }
+        }        
+    }
+}
+class Enemy extends Character {
+    Enemy(String name, int win, int lose) {
+        super(name, win, lose);
+    }
+ 
+    @Override
+    void act(ArrayList<Character> targets) {
+        Random random = new Random();
+        int command = random.nextInt(3);
+        getHands().get(command);
+    }
+ }
+
+ class Rock implements Hand{
+    
+ }
